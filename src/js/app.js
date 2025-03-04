5a81d31d6d05972f0db17f7e4b7b9554f8bb4dbb
lianet <jack.tolmak@yandex.ru>
2025-03-04 15:40:00 +0300

    commit

--- src/js/app.js
+++ src/js/app.js
@@ -23,77 +23,75 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 
-
-
     /* event handlers */
     document.addEventListener("click", (e) => {
 
         const target = e.target;
 
+        if (target.matches('.search__btn')) {
+            document.querySelector('.search').classList.add('active');
+            setTimeout(() => {
+                document.querySelector('.search__form-input')?.focus()
+            }, 300)
+        }
 
-
-        if (target.matches('.search__toggler')) {
-
+        if (!target.closest('.search') && document.querySelector('.search').classList.contains('active')) {
+            document.querySelector('.search').classList.remove('active');
         }
 
 
     });
 
+
+
+
+
+    document.querySelector('.search')?.addEventListener('reset', (e) => {
+        document.querySelector('.search__form-reset').classList.remove('active');
+    });
+
+    document.querySelector('.search__form-input')?.addEventListener('input', (e) => {
+        if (e.target.value.length > 0) {
+            document.querySelector('.search__form-reset').classList.add('active');
+        } else {
+            document.querySelector('.search__form-reset').classList.remove('active');
+        }
+    });
+
+
+
     // sliders
 
-    if (document.querySelector('.reviews__slider')) {
-        new Swiper('.reviews__slider', {
-            slidesPerView: 1.15,
-            spaceBetween: 20,
+    if (document.querySelector('.gallery__slider')) {
+        new Swiper('.gallery__slider .swiper', {
+            slidesPerView: 1,
+            spaceBetween: 27,
             watchSlidesProgress: true,
             navigation: {
-                nextEl: ".reviews__next",
-                prevEl: ".reviews__prev",
-            },
-            breakpoints: {
-                767.98: {
-                    slidesPerView: 2,
-                }
-            }
-        })
-    }
-
-    if (document.querySelector('.reviews__more-slider')) {
-        new Swiper('.reviews__more-slider', {
-            slidesPerView: "auto",
-            spaceBetween: 20,
-            watchSlidesProgress: true,
-            pagination: {
-                el: '.reviews__more-pagination',
-                clickable: true
+                nextEl: ".gallery__next",
+                prevEl: ".gallery__prev",
             }
         })
     }
 
 
-    // document.addEventListener('scroll', (e) => {
-    //     if (scrollY > 0) {
-    //         document.querySelector('.header').classList.add('scroll');
-    //     } else {
-    //         document.querySelector('.header').classList.remove('scroll');
-    //     }
-    // });
 
 
-    // // observer header height
-    // function updateHeaderHeight() {
-    //     var header = document.querySelector('.header__wrapper');
-    //     var htmlFontSize = parseFloat(getComputedStyle(document.documentElement).fontSize);
 
-    //     if (header && htmlFontSize) {
-    //         var headerHeight = header.offsetHeight / htmlFontSize;
-    //         document.body.style.setProperty('--header-height', `${headerHeight}rem`);
-    //     }
-    // }
+    // observer header height
+    function updateHeaderHeight() {
+        var header = document.querySelector('.header__wrapper');
+        var htmlFontSize = parseFloat(getComputedStyle(document.documentElement).fontSize);
 
-    // updateHeaderHeight();
+        if (header && htmlFontSize) {
+            var headerHeight = header.offsetHeight / htmlFontSize;
+            document.body.style.setProperty('--header-height', `${headerHeight}rem`);
+        }
+    }
 
-    // window.addEventListener('resize', updateHeaderHeight);
+    updateHeaderHeight();
+
+    window.addEventListener('resize', updateHeaderHeight);
 
 
 
